9f4e067e1086524659cc3950c85fda21c9e332ec
Radhika Bhoyar <bhoyarradhika17@gmail.com>
2025-03-02 18:09:32 +0530

    Declutter

--- userapp/lib/global/global_var.dart
+++
@@ -1,9 +0,0 @@
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-String userName = "";
-String googleMapKey = "AIzaSyCKZoJOvVzRDu-vypMCOpCgTjkxStRSmRA";
-
-const CameraPosition googlePlexInitialPosition = CameraPosition(
-  target: LatLng(37.42796133580664, -122.085749655962),
-  zoom: 14.4746,
-);
